06f6dea9f11a0b92bcd5248a3aa310594136ce0f
liatVich <liatvi@wix.com>
2022-02-07 12:13:26 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-function App() {
+import React from 'react'
+import './App.scss';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Outlet, Link } from "react-router-dom";
+import { routes } from "./routes"
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+    <Typography variant="h5" gutterBottom component="div">
+    Main App Page
+    </Typography>
+    <Button variant="contained">SOME BUTTON</Button>
+          <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to={'/' + routes.CREATE_EXPERIMENT}>CreateExperiment</Link>
+      </nav>
+      <Outlet/>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/routes/consts.js
@@ -0,0 +1,3 @@
+export const routes = {
+    CREATE_EXPERIMENT: 'create',
+}
