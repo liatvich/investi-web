82abc54d1c0679a857ba307d638e6d357ee86a5b
liatv <liatvi@wix.com>
2024-04-16 09:14:57 +0300

    improve text

--- src/components/App/Preview/PreviewReadText.jsx
+++ src/components/App/Preview/PreviewReadText.jsx
@@ -12,12 +12,44 @@ export function PreviewReadText({ node, disabled }) {
   const [value, setValue] = useState(node?.attrs?.value);
   const [currDisabled, setCurrDisabled] = useState(disabled);
 
+
+  async function imageProcess(file){
+    // Load image and convert it to an image object
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+    
+    // Wait for the image to load
+    await new Promise(resolve => {
+      img.onload = resolve;
+    });
+  
+    // Create a canvas to draw the enlarged image
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+  
+    const upscaleBy = 2
+    // Enlarge the image by doubling its size
+    canvas.width = img.width * upscaleBy;
+    canvas.height = img.height * upscaleBy;
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  
+    // Convert canvas to Blob object
+    const blob = await new Promise(resolve => canvas.toBlob(resolve, 'image/jpeg', 1));
+  
+    // Use Tesseract.js to recognize the text in the enlarged image
+    const result = await Tesseract.recognize(blob, 'eng');
+    const text = result?.data?.text;
+    
+    return text;
+  }
+
+
   const handleImageUpload = async (event) => {
     setCurrDisabled(true);
 try {
     if (event?.target?.files?.[0]) {
-      const result = await Tesseract.recognize(URL.createObjectURL(event.target.files[0]), 'eng');
-      setValue(result?.data?.text);
+      const text = await imageProcess(event.target.files[0])
+      setValue(text);
       // eslint-disable-next-line no-param-reassign
       if (node?.attrs) {
         // eslint-disable-next-line no-param-reassign
